10e97573441f254705e7696d58b2632c91905d49
kattoc15 <katarzynatoczek15@gmail.com>
2019-12-15 12:28:34 +0100

    First commit.

---
+++ src/main/java/pl/coderslab/gabinet_fizjoterapii/GabinetFizjoterapiiApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.gabinet_fizjoterapii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabinetFizjoterapiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabinetFizjoterapiiApplication.class, args);
+    }
+
+}
